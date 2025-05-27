7007795094b386971290cea61f1a20b302f935de
NicolasLMP <nicolas.lambropoulos54@gmail.com>
2025-05-27 15:36:55 +0200

    Merged main

--- src/help_desk.py
+++ src/help_desk.py
@@ -5,6 +5,7 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_openai import ChatOpenAI
+from langchain_core.utils.utils import convert_to_secret_str
 import os
 
 
@@ -63,7 +64,7 @@ class HelpDesk:
         return embeddings
 
     def get_llm(self):
-        api_key = os.getenv("OPENROUTER_API_KEY")
+        api_key = convert_to_secret_str(os.getenv("OPENROUTER_API_KEY", ""))
         if not api_key:
             raise ValueError("OPENROUTER_API_KEY not found in environment variables")
 
