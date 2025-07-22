02ca4e9e99033167fad5fff755de814317a04c1f
NicolasLMP <nicolas.lambropoulos54@gmail.com>
2025-07-22 18:08:18 +0200

    feat : first proto

---
+++ src/webapp/example_prompts.py
@@ -0,0 +1,6 @@
+# src/webapp/example_prompts.py
+
+EXAMPLE_PROMPTS = [
+    {"title": "Identifier une équipe projet", "prompt": "Quelle équipe travaille sur le projet Isschat ?"},
+    {"title": "Résumer un événement", "prompt": "Que s’est-il passé lors de la matinale du 2 Juillet ?"},
+]
